b57759e3ba578eb5c905ee0ef8a57d5e7fb9c4b6
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-18 20:51:55 -0300

    Incrementado a variável "tipoLong" na classe "Variaveis"

--- curso-java/src/variaveis/Variaveis.java
+++ curso-java/src/variaveis/Variaveis.java
@@ -5,13 +5,14 @@ import java.math.BigDecimal;
 public class Variaveis {
 	public static void main(String[] args) {
 		System.out.println("Váriaveis em java");
-		byte tipoByte = Byte.MAX_VALUE;
+		byte tipoByte = -(Byte.MAX_VALUE + 1);
 		short tipoShort = Short.MAX_VALUE;
 		int tipoInt = Integer.MAX_VALUE;
 		boolean tipoBoolean = true;
 		String tipoTexto = "Hello, Java";
 		BigDecimal tipoBigDecimal = BigDecimal.ONE;
 		double tipoDouble = Double.MAX_VALUE;
+		long tipoLong = Long.MAX_VALUE;
 
 		System.out.println(tipoByte);
 		System.out.println(tipoShort);
@@ -20,5 +21,6 @@ public class Variaveis {
 		System.out.println(tipoTexto);
 		System.out.println(tipoDouble);
 		System.out.println(tipoBigDecimal);
+		System.out.println(tipoLong);
 	}
 }
