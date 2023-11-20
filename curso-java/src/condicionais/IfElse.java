fd4ec0530342e832688ce5838b034470bc1db3a6
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-20 11:56:45 -0300

    if aninhado adicionado

--- curso-java/src/condicionais/IfElse.java
+++ curso-java/src/condicionais/IfElse.java
@@ -2,11 +2,11 @@ package condicionais;
 
 public class IfElse {
 	public static void main(String[] args) {
-		double nota1 = 300;
-		double nota2 = 300;
-		double nota3 = 300;
-		double nota4 = 300;
-		double redacao = 700;
+		double nota1 = 900;
+		double nota2 = 900;
+		double nota3 = 900;
+		double nota4 = 901;
+		double redacao = 900;
 		double media = (nota1 + nota2 + nota3 + nota4 + redacao) / 5;
 		
 		if (media >= 500 && redacao >= 700) {
@@ -15,6 +15,21 @@ public class IfElse {
 			System.out.println("Aluno em recuperação com a média: " + media);
 		} else {
 			System.out.println("Aluno reprovado com a média: " + media);
+		}
+		
+		// operações lógicas aninhadas
+		if (media >= 500) {
+			if (media >= 700) {
+				if (media > 900) {
+					System.out.println("Aluno aprovado direto 1");
+				} else {
+					System.out.println("Aluno aprovado direto 2");
+				}
+			} else {
+				System.out.println("Aluno em recuperação");
+			}
+		} else {
+			System.out.println("Aluno reprovado");
 		}
 	}
 }
