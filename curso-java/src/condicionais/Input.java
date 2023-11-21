52d2296a1ab809cbb415e577c37074b367f843d3
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-21 17:16:03 -0300

    Adicionado um método de confirmação na classe "Input"

--- curso-java/src/condicionais/Input.java
+++ curso-java/src/condicionais/Input.java
@@ -7,14 +7,21 @@ public class Input {
 		try {
 			String carros = JOptionPane.showInputDialog("Informe a quantidade de carros");
 			String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas");
-			
+
 			double carroNumero = Double.parseDouble(carros);
 			double pessoaNumero = Double.parseDouble(pessoas);
-			
+
 			int divisao = (int) (carroNumero / pessoaNumero);
 			double resto = carroNumero % pessoaNumero;
 
-			JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carros");
+			int res = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão?");
+
+			if (res == 0) {
+				JOptionPane.showMessageDialog(null,
+						"Divisão para pessoas deu " + divisao + " carros e sobrou " + resto + " carros");
+			} else {
+				JOptionPane.showMessageDialog(null, "Saindo do programa");
+			}
 		} catch (NullPointerException error) {
 			System.out.println(error);
 		}
