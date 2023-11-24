de561602639b692545b1c4f1a34fc1205b8650e9
Ítalo Oliveira <italo.ods@hotmail.com>
2023-11-24 16:43:43 -0300

    Implementado o sistema de lista de alunos

--- curso-java/src/poo/App.java
+++ curso-java/src/poo/App.java
@@ -1,5 +1,8 @@
 package poo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 import poo.classes.Aluno;
 import poo.classes.Disciplina;
@@ -7,57 +10,61 @@ import poo.classes.Disciplina;
 public class App {
 	public static void main(String[] args) {
 
-		String nome = JOptionPane.showInputDialog(null, "Qual o nome do aluno?");
-//		int idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual a idade do aluno?"));
-//		String dataNascimento = JOptionPane.showInputDialog(null, "Qual a data de nascimento?");
-//		String rg = JOptionPane.showInputDialog(null, "Qual o seu RG?");
-//		String cpf = JOptionPane.showInputDialog(null, "Qual seu CPF?");
-//		String nomeMae = JOptionPane.showInputDialog(null, "Qual o nome da mãe?");
-//		String nomePai = JOptionPane.showInputDialog(null, "Qual o nome do pai?");
-//		String dataMatricula = JOptionPane.showInputDialog(null, "Qual a data da martrícula?");
-//		String serieMatriculado = JOptionPane.showInputDialog(null, "Qual a série matrículada?");
-//		String nomeEscola = JOptionPane.showInputDialog(null, "Qual o nome da escola?");
-		int numeroDisciplinas = Integer.parseInt(JOptionPane.showInputDialog(null, "Quantas disciplinas você tem?"));
+		List<Aluno> alunos = new ArrayList<Aluno>();
 
-		Aluno aluno1 = new Aluno();
+		for (int qtd = 1; qtd <= 2; qtd++) {
 
-		for (int i = 1; i <= numeroDisciplinas; i++) {
-			String nomeDisciplina = JOptionPane.showInputDialog(null, "Qual o nome da disciplina " + i);
-			double notaDisciplina = Double
-					.parseDouble(JOptionPane.showInputDialog(null, "Qual a nota da disciplina" + i));
-			aluno1.getDisciplinas().add(new Disciplina(nomeDisciplina, notaDisciplina));
+			String nome = JOptionPane.showInputDialog(null, "Qual o nome do aluno " + qtd + "?");
+//			int idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual a idade do aluno?"));
+//			String dataNascimento = JOptionPane.showInputDialog(null, "Qual a data de nascimento?");
+//			String rg = JOptionPane.showInputDialog(null, "Qual o seu RG?");
+//			String cpf = JOptionPane.showInputDialog(null, "Qual seu CPF?");
+//			String nomeMae = JOptionPane.showInputDialog(null, "Qual o nome da mãe?");
+//			String nomePai = JOptionPane.showInputDialog(null, "Qual o nome do pai?");
+//			String dataMatricula = JOptionPane.showInputDialog(null, "Qual a data da martrícula?");
+//			String serieMatriculado = JOptionPane.showInputDialog(null, "Qual a série matrículada?");
+//			String nomeEscola = JOptionPane.showInputDialog(null, "Qual o nome da escola?");
+
+			Aluno aluno = new Aluno(nome);
+//			Aluno aluno = new Aluno(nome, idade, dataNascimento, rg, cpf, nomeMae, nomePai, dataMatricula, serieMatriculado, nomeEscola);
+
+			int numeroDisciplinas = Integer
+					.parseInt(JOptionPane.showInputDialog(null, "Quantas disciplinas você tem?"));
+
+			for (int i = 1; i <= numeroDisciplinas; i++) {
+				String nomeDisciplina = JOptionPane.showInputDialog(null, "Qual o nome da disciplina " + i);
+				double notaDisciplina = Double
+						.parseDouble(JOptionPane.showInputDialog(null, "Qual a nota da disciplina" + i));
+				aluno.getDisciplinas().add(new Disciplina(nomeDisciplina, notaDisciplina));
+			}
+
+			int removerDisciplina = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+			if (removerDisciplina == 0) {
+				int continuarRemover = 0;
+				do {
+					if (aluno.getDisciplinas().size() > 1) {
+						String nomeDisciplina = JOptionPane.showInputDialog(null,
+								"Qual o nome da disciplina a ser removida?");
+						Disciplina disciplinaRemovida = aluno.removeDisciplina(nomeDisciplina);
+						System.out.println("Disciplina removida = " + disciplinaRemovida);
+						continuarRemover = JOptionPane.showConfirmDialog(null,
+								"Deseja continuar removendo disciplinas?");
+					} else {
+						JOptionPane.showMessageDialog(null, "Você deve ter pelo menos uma disciplina cadastrada");
+						continuarRemover = -1;
+					}
+				} while (continuarRemover == 0);
+			}
+			alunos.add(aluno);
 		}
 
-		int removerDisciplina = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-		if (removerDisciplina == 0) {
-			int continuarRemover = 0;
-			do {
-				if (aluno1.getDisciplinas().size() > 1) {
-					String nomeDisciplina = JOptionPane.showInputDialog(null,
-							"Qual o nome da disciplina a ser removida?");
-					aluno1.removeDisciplina(nomeDisciplina);
-					continuarRemover = JOptionPane.showConfirmDialog(null, "Deseja continuar removendo disciplinas?");
-				} else {
-					JOptionPane.showMessageDialog(null, "Você deve ter pelo menos uma disciplina cadastrada");
-					continuarRemover = -1;
-				}
+		System.out.println(alunos);
 
-			} while (continuarRemover == 0);
+		for (Aluno aluno : alunos) {
+			System.out.println(aluno.getNome() + " - Média = " + aluno.getMedia());
+			System.out.println(aluno.getNome() + " - Resultado = " + aluno.verificarAprovacaoStr());
 		}
 
-		aluno1.setNome(nome);
-//		aluno1.setIdade(idade);
-//		aluno1.setDataMatricula(dataMatricula);
-//		aluno1.setDataNascimento(dataNascimento);
-//		aluno1.setNomeEscola(nomeEscola);
-//		aluno1.setNomeMae(nomeMae);
-//		aluno1.setNomePai(nomePai);
-//		aluno1.setNumeroCpf(cpf);
-//		aluno1.setSerieMatriculado(serieMatriculado);
-//		aluno1.setRegistroGeral(rg);
-
-		System.out.println(aluno1.toString());
-		System.out.println("Média = " + aluno1.getMedia());
-		System.out.println("Resultado = " + aluno1.verificarAprovacaoStr());
+//		System.out.println(aluno.toString());
 	}
 }
--- curso-java/src/poo/classes/Aluno.java
+++ curso-java/src/poo/classes/Aluno.java
@@ -30,9 +30,22 @@ public class Aluno {
 	// construtores
 	public Aluno() {
 	}
-
+	
 	public Aluno(String nome) {
 		this.nome = nome;
+	}
+
+	public Aluno(String nome, int idade, String dataNascimento, String registroGeral, String numeroCpf, String nomeMae, String nomePai, String dataMatricula, String nomeEscola, String serieMatriculado) {
+		this.nome = nome;
+		this.idade =idade;
+		this.dataNascimento = dataNascimento;
+		this.registroGeral  = registroGeral;
+		this.numeroCpf = numeroCpf;
+		this.nomeMae = nomeMae;
+		this.nomePai = nomePai;
+		this.dataMatricula = dataMatricula;
+		this.nomeEscola = nomeEscola;
+		this.serieMatriculado = serieMatriculado;
 	}
 
 	// métodos
