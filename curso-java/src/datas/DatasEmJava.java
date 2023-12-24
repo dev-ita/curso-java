4e2d52cad312dacc839de427a22a71e76f9da55f
dev-ita <italo.ods@hotmail.com>
2023-12-23 21:12:45 -0300

    Utilizando o objeto Calendar

--- curso-java/src/datas/DatasEmJava.java
+++ curso-java/src/datas/DatasEmJava.java
@@ -19,19 +19,29 @@ public class DatasEmJava {
 		
 		System.out.println("-----------------------------------");
 		
-		Calendar data2 = Calendar.getInstance();
-		System.out.println(data2.getTime());
+		Calendar calendar = Calendar.getInstance();
+		System.out.println(calendar.getTime());
 		
 		/*Simple date format*/
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
 		System.out.println(simpleDateFormat.format(data1));
 		System.out.println(simpleDateFormat2.format(data1));
-		System.out.println(simpleDateFormat.format(data2.getTime()));
+		System.out.println(simpleDateFormat.format(calendar.getTime()));
 		
 		System.out.println(simpleDateFormat.parse("21/02/2004 23:55:20"));
 
 		SimpleDateFormat simpleDateFormat3 = new SimpleDateFormat("yyyy/MM/dd");
-		System.out.println(simpleDateFormat3.parseObject("1987/02/21"));
+		System.out.println(simpleDateFormat3.parse("1987/02/21"));
+		System.out.println("------------ Calendar ------------");
+		System.out.println("Data em milisegundos: " + calendar.getTimeInMillis());
+		System.out.println("Mês: " + (calendar.get(Calendar.MONTH) + 1));
+		System.out.println("Dia da semana: " + calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println("Hora do dia: " + calendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println("Minuto da hora: " + calendar.get(Calendar.MINUTE));
+		System.out.println("Segundos: " + calendar.get(Calendar.SECOND));
+		System.out.println("Ano: " + calendar.get(Calendar.YEAR));
+		System.out.println("Formataçao: " + simpleDateFormat.format(calendar.getTime()));
+		System.out.println(calendar);
 	}
 }
